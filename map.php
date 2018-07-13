a58d37d21eaab6e56b690dc1b829e94ba40d9d76
bruno vianna <bruno@pobox.com>
2018-07-13 18:43:14 -0300

    changed mousenter to mouseleave

--- map.php
+++ map.php
@@ -140,7 +140,7 @@ question.push( <?php echo $_POST["question_8"]; ?> );
 </div>
 
 <div class="fence_us" id="fence_us_id"><span class="futura-18">ESTADOS UNIDOS</span></div>
-<div class="fence" id="fence_id" onmouseover="show_fence_text();" onmouseout="hide_fence_text();" onclick="click_fence();">
+<div class="fence" id="fence_id" onmouseenter="show_fence_text();" onmouseleave="hide_fence_text();" onclick="click_fence();">
   <img src="images/fronteira_amarela.png">
 </div>
 <div class="fence_mexico"  id="fence_mexico_id"><span class="futura-18">MÉXICO</span></div>
